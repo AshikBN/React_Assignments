b2d6131ab4c6fd3fb66d664043eba951d0fb83f6
Your Name <you@example.com>
2022-04-28 18:16:59 +0530

    completed assignment 3

---
+++ Assignment-3/code/my-app/src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const employee = {
+  arrow: "<",
+  name: "Alan Ford",
+  id: "000005152",
+  appointment: "9:00(24-05-2016)",
+  email: "alan.form@gmail.com",
+  phone: +31123456789,
+  status: "In Progress",
+  door: "Mark",
+  time: "10:30(25-05-2016)",
+  profileImg: "https://www.w3schools.com/howto/img_avatar.png",
+  role: "Boltart Bosbessen",
+  work: "This is some random description about the product mentioned above.",
+  arrow1: ">",
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App employee={employee} />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
